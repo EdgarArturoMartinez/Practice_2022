68bf5e081e5254a75a2b02045a11007a5f53ef05
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-01-13 10:47:48 -0500

    First Commit. Creating Console Menu

---
+++ ConsoleExercises/ConsoleExercises/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Menu with to check available exercises
+            Console.WriteLine("Welcome to Available Apps");
+            Console.WriteLine("===========================================");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Show Basic formats");
+            Console.WriteLine("1.  Show Datetimes formats");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+    }
+}
